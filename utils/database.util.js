bf30032caa38cd0426d58d01d3f4453a938f4811
EdgarMediaz <102551598+EdgarMediaz@users.noreply.github.com>
2022-09-12 14:59:08 -0700

    created env file

--- utils/database.util.js
+++ utils/database.util.js
@@ -1,12 +1,15 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 const db = new Sequelize({
-	host: 'localhost',
-	username: 'postgres',
-	password: 'Emed1213',
-	database: 'tasks',
-	port: 5432,
 	dialect: 'postgres',
+	host: process.env.DB_HOST,
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+	database: process.env.DB,
 	logging: false,
 });
 
